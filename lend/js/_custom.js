45aaa0d74c07370123d6c5445c3ea14c1e875b4b
eridiante <51819977+eridiante@users.noreply.github.com>
2019-08-26 01:14:51 +0300

    Add files via upload

---
+++ lend/js/_custom.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function() {
+	$( "#accordion" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_1" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_2" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_3" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_4" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_5" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_6" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_7" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+	$( "#accordion_8" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+		active: false
+	});
+
+});
